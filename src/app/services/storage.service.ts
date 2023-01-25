fe06502a775ca47376e978e028a5751c69dcbf2c
JoseMGMurcia <guigatboise@gmail.com>
2023-01-25 13:11:49 +0100

    feat: added storage

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { DATABASE_NAME } from '../constants/constants';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+ private uStorage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    this.uStorage = storage;
+    const characters =  await this.storage.get(DATABASE_NAME);
+    if (null === characters){
+     await this.uStorage.set(DATABASE_NAME, JSON.stringify([]));
+    }
+  }
+
+  public async set(key: string, value: any) {
+    await this.uStorage?.set(key, value);
+  }
+
+  public async get(key: string){
+    return await this.storage.get(key);
+  }
+}
